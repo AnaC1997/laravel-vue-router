85fb7eda5ae70902cc8af5e4461ee5795b19c33c
Ana Maria Correa <111539281+AnaC1997@users.noreply.github.com>
2024-02-12 17:56:25 +0100

    visualizzazione proprietà tag in ruta CategoriService

--- src/pages/CategoryService.vue
+++ src/pages/CategoryService.vue
@@ -1,23 +1,56 @@
 <script>
+import axios from "axios";
+import { store } from "../store" //state management
+
 export default {
     name: "CategoryService",
+    components: {
+
+
+    },
+
+    data() {
+        return {
+            store,
+            tags: null
+        }
+    },
+    mounted() {
+        this.getTags();
+
+    },
+    methods: {
+        getTags() {
+            let url = this.store.apiUrl + this.store.apiTag;
+            axios.get(url).then(risultato => {
+                console.log("url", url);
+                if (risultato.data.success == true) {
+                    this.store.tags = risultato.data.payload;
+                    console.log("Risultato: ", risultato.data.payload);
+                }
+            });
+
+        }
+
+
+    }
 }
 </script>
 
 <template>
     <div class="container">
-        <div class="row">
-           <h1>Servici 1</h1>
-           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ipsum consectetur eligendi, repudiandae neque aperiam, praesentium earum dicta doloremque inventore, adipisci amet et veritatis dignissimos perspiciatis necessitatibus? Sunt, repellat quibusdam?</p>
+        <div class="row justify-content-center">
+            <div class="col-4"  v-for="tag in store.tags">
+                <div class="card mb-3" style="width: 23rem; height: 23rem;">
+                <img :src="tag.img " class="card-img-top"  alt="..." style=" height: 15rem;" >
+                <div class="card-body">
+                    <h5 class="card-title">{{ tag.name }}</h5>
+                    <a href="#" class="btn btn-primary">Vizualiza gli eventi</a>
+                </div>
+            </div>
 
-           <h1>Servici 2</h1>
-           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ipsum consectetur eligendi, repudiandae neque aperiam, praesentium earum dicta doloremque inventore, adipisci amet et veritatis dignissimos perspiciatis necessitatibus? Sunt, repellat quibusdam?</p>
-
-           <h1>Servici 3</h1>
-           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ipsum consectetur eligendi, repudiandae neque aperiam, praesentium earum dicta doloremque inventore, adipisci amet et veritatis dignissimos perspiciatis necessitatibus? Sunt, repellat quibusdam?</p>
-
-           <h1>Servici 4</h1>
-           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ipsum consectetur eligendi, repudiandae neque aperiam, praesentium earum dicta doloremque inventore, adipisci amet et veritatis dignissimos perspiciatis necessitatibus? Sunt, repellat quibusdam?</p>
+            </div>
+           
 
         </div>
     </div>
